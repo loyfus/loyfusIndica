1bb12f895f70ad66ccb5b156c58b1da3aef72534
loyfus <felipepenha32@gmail.com>
2024-11-03 14:54:21 -0300

    Main

---
+++ src/main/java/online/loyfus/loyfus_online/LoyfusOnlineApplication.java
@@ -0,0 +1,13 @@
+package online.loyfus.loyfus_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyfusOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyfusOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/loyfus/loyfus_online/LoyfusOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package online.loyfus.loyfus_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyfusOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
